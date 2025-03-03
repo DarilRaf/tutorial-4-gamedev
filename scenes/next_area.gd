d8873c175463c32218b4c65b5246c4ea7dcaae5b
DarilRaf <darilmrafansyah@gmail.com>
2025-03-03 12:03:56 +0700

    Latihan Mandiri Done

---
+++ scenes/next_area.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var player: CharacterBody2D = $"../Player"
+var camera: Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	camera = player.get_node("Camera2D")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.get_name() == "Player":
+		camera.limit_right = 1728
+		camera.limit_bottom = 680
+		camera.limit_top = -200
