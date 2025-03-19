14122ac6312583a7dc4abbe77af006e8fa17ef60
DarilRaf <darilmrafansyah@gmail.com>
2025-03-20 01:11:08 +0700

    Fixes to level and logic

--- scenes/back_menu.gd
+++ scenes/back_menu.gd
@@ -3,5 +3,6 @@ extends LinkButton
 @export var scene_to_load: String
 
 func _on_pressed() -> void:
+	Global.lives = 3
 	var scene_path = "res://scenes/%s.tscn" % scene_to_load
 	get_tree().change_scene_to_file(scene_path)
